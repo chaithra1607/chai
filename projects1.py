a41de86017cb67f66507de55fc6d4d797959f9cb
chaithra1607 <chaismile88@gmail.com>
2024-08-03 17:03:37 +0530

    lite

---
+++ projects1.py
@@ -0,0 +1,20 @@
+#calculator
+''' def adds(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def multi(a,b):
+    print(a*b)
+def division(a,b):
+    print(a/b)
+a=int(input("enter the numbers"))
+b=int(input("enter the number2"))
+c=input(f"enter either add  or sub or division or multi").lower()
+if(c=="add"):
+    adds(a,b)
+if c=="sub":
+    sub(a,b)
+if c=="multi":
+    multi(a,b)
+if c=="division":
+    division(a,b)'''
